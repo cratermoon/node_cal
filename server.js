b89bdd3d112814fc2181c02130e2fc6153dced77
Steven E. Newton <cratermoon@gmail.com>
2016-08-25 07:27:31 +0000

    added a flickr image feed

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const urlmodule = require('url');
 const quips = require('./lib/quips');
 quips.loadQuips();
 
-
 const renderer = require('./lib/render');
+
+const flickrr = require('./lib/flickrr');
 
 http.createServer(function(request, response) {
   request.on('error', function(err) {
@@ -20,16 +21,22 @@ http.createServer(function(request, response) {
   });
   if (request.method === 'GET') {
     var parsedUrl = urlmodule.parse (request.url, true);
-    var lowerpath = parsedUrl.pathname.toLowerCase ();
-    console.log("path is "+lowerpath);
-    if (lowerpath == "/") {
+    var path = parsedUrl.pathname.toLowerCase();
+    console.log("path is "+path);
+    if (path == "/") {
       var  clientIp = request.connection.remoteAddress;
       response.setHeader('Content-Type', 'text/plain');
       response.write(quips.randomQuip());
       response.write("\n");
       response.end();
+    } else if (path == "/flickr") {
+      flickrr.flickrl(function(result) {
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify(result));
+        response.end();
+      });
     } else {
-      renderer.render(response, lowerpath);
+      renderer.render(response, path);
     }
   } else {
     response.statusCode = 404;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 var quips = require('../lib/quips');
 quips.loadQuips();
 
+var flickrr = require('../lib/flickrr');
+
 describe('Quips Test', function() {
   describe('#randomQuip', function() {
     it('should return a non-empty string', function() {
@@ -11,3 +13,18 @@ describe('Quips Test', function() {
     });
   });
 });
+
+describe('Flickr Test', function() {
+  describe('#flickrl', function() {
+    it('should return a json object', function(done) {
+      flickrr.flickrl(function(result) {
+        assert.ok(result.farm, "no farm");
+        assert.ok(result.server, "no server");
+        assert.ok(result.id, "no id");
+        assert.ok(result.secret, "no secret");
+        done();
+      });
+    });
+  });
+});
+
